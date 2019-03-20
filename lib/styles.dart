5149a28ae159ba4b1bc0eeb49346d693e3ca232c
Wade Garrett <wade@wadegarrett.com>
2019-03-20 12:48:08 -0500

    add styles

---
+++ lib/styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+Color bgColor =
+    const Color(0xFF6616C5).withOpacity(0.8); // coralish#FC9E97coralish
+Color primaryColor = const Color(0xFFF7D838); // yellow
+Color secondaryColor = const Color(0xFFF68282); // redish
+Color tertiaryColor = const Color(0xFF8ADB68); // greenish
+
+TextStyle buttonTextStyle = const TextStyle(
+  color: const Color.fromRGBO(255, 255, 255, 0.8),
+  fontSize: 16.0,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle headerText = const TextStyle(
+  fontSize: 48.0,
+  fontWeight: FontWeight.w800,
+  color: const Color(0xFFF7D838),
+);
+
+TextStyle infoTextStyle = const TextStyle(
+  color: const Color(0XFFFFFFFF),
+  fontSize: 16.0,
+  height: 1.8,
+  fontWeight: FontWeight.normal,
+);
+
+TextStyle infoLinkStyle = const TextStyle(
+  color: const Color(0xFFF7D838),
+  fontSize: 18.0,
+  height: 1.8,
+  fontWeight: FontWeight.w500,
+  decoration: TextDecoration.underline
+);
+
+TextStyle infoTitleStyle = const TextStyle(
+  color: const Color(0XFFFAFAFA),
+  fontSize: 20.0,
+  height: 1.0,
+  fontWeight: FontWeight.w600,
+);
