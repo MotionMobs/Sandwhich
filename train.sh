bf58efacf8effb739e08ce9b4baca45675ba968b
Matthew Clark <mclark4386@gmail.com>
2019-03-21 00:31:32 -0500

    *absolutely

--- train.sh
+++ train.sh
@@ -12,10 +12,10 @@ docker build -t sandwiching tf_files/
 echo "Retraining model with given images..."
 docker run -it \
   -v $(pwd)/tf_files:/tf_files \
-  -v $(pwd)/training_images/output:/input sandwiching python3 tf_files/retrain.py \
-  --bottleneck_dir=tf_files/bottlenecks \
+  -v $(pwd)/training_images/output:/input sandwiching python3 /tf_files/retrain.py \
+  --bottleneck_dir=/tf_files/bottlenecks \
   --how_many_training_steps="${TRAINING_STEPS}" \
-  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --summaries_dir=/tf_files/training_summaries/"${ARCHITECTURE}" \
   --output_graph="${GRAPH}" \
   --output_labels="${OUTPUT_LABELS}" \
   --architecture="${ARCHITECTURE}" \
