95cc8eff9f5779646eadb328692ea957de8f6421
Taylor Smith <taylorsmith@Taylors-MacBook-Pro-2.local>
2019-03-21 08:39:34 -0500

    removing input shape stuff for now

--- train.sh
+++ train.sh
@@ -29,8 +29,8 @@ docker run -it \
   --output_file=${OUTPUT} \
   --input_format=TENSORFLOW_GRAPHDEF \
   --output_format=TFLITE \
-  --input_shape=1,${IMAGE_SIZE},${IMAGE_SIZE},3 \
-  --input_array=input \
   --output_array=final_result \
   --inference_type=FLOAT \
   --input_data_type=FLOAT
+# --input_shape=1,${IMAGE_SIZE},${IMAGE_SIZE},3 \
+# --input_array=input \
