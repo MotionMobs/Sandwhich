2b17ff95c2fd809dde8cd44b134f20811dc7ac2c
Matthew Clark <mclark4386@gmail.com>
2019-03-20 19:11:13 -0500

    *moved the training out of the build of the image, so the image is just the env *added echos for knowing what step we are on in training

--- train.sh
+++ train.sh
@@ -2,13 +2,26 @@
 IMAGE_SIZE=224
 GRAPH=/tf_files/retrained_graph.pb
 OUTPUT=/tf_files/sandwich.tflite
+OUTPUT_LABELS=/tf_files/retrained_labels.txt
+ARCHITECTURE=mobilenet_0.50_${IMAGE_SIZE}
+TRAINING_STEPS=500
 
+echo "Building docker image..."
 docker build -t sandwiching tf_files/
 
+echo "Retraining model with given images..."
 docker run -it \
   -v $(pwd)/tf_files:/tf_files \
-  -v $(pwd)/training_images/output:/input sandwiching
+  -v $(pwd)/training_images/output:/input sandwiching python3 tf_files/retrain.py \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --how_many_training_steps="${TRAINING_STEPS}" \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph="${GRAPH}" \
+  --output_labels="${OUTPUT_LABELS}" \
+  --architecture="${ARCHITECTURE}" \
+  --image_dir=/input
 
+echo "Now to convert this puppy!"
 docker run -it \
   -v $(pwd)/tf_files:/tf_files \
   -v $(pwd)/training_images/output:/input sandwiching tflite_convert \
